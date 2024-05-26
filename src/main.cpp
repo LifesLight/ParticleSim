983aecc1815813feefafa4f515bc7cb63b48a93e
Alexander Kurtz <alexkurtz@web.de>
2024-05-26 14:31:16 +0200

    Init Repo

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include <stdio.h>
+
+int main() {
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to create window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
